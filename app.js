3218b81fa95baaa242900635fa92bd1d24d09849
Feral Satyam <satyamranaofficiall@gmail.com>
2025-11-24 17:31:41 +0545

    Updated UI

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const studentModel = require("./models/student")
+const teacherModel = require("./models/teacher")
 const bcrypt = require('bcrypt')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
@@ -18,6 +19,14 @@ function isLoggedInStudent(req, res, next){
         next()
     } 
 }
+function isLoggedInTeacher(req, res, next){
+    if(req.cookies.token === "") res.redirect("/teacherlogin");
+    else{
+        let data = jwt.verify(req.cookies.token, "secret")
+        req.teacher = data
+        next()
+    } 
+}
 
 app.get('/', (req, res)=>{
     res.render("index")
@@ -25,22 +34,32 @@ app.get('/', (req, res)=>{
 app.get('/studentlogin', (req, res)=>{
     res.render("studentlogin")
 })
+app.get('/teacherlogin', (req, res)=>{
+    res.render("teacherlogin")
+})
 app.get('/admin', (req, res)=>{
     res.render("admin")
 })
 app.get('/studentregister', (req, res)=>{
     res.render("studentregister")
 })
+app.get('/teacherregister', (req, res)=>{
+    res.render("teacherregister")
+})
 app.get('/studentprofile', isLoggedInStudent, async (req, res)=>{
     let student = await studentModel.findOne({email: req.student.email})
     res.render("studentprofile", {student})
 })
+app.get('/teacherprofile', isLoggedInTeacher, async (req, res)=>{
+    let teacher = await studentModel.findOne({email: req.teacher.email})
+    res.render("teacherprofile", {teacher})
+})  
 
 
 app.post('/studentlogin', async (req, res)=>{
     let {email, password} = req.body
     let student = await studentModel.findOne({email})
-    if(!student) res.status(500).send("Something went wrong")
+    if(!student) return res.status(500).send("Something went wrong")
     
     bcrypt.compare(password, student.password, (err, result)=>{
         if(result){
@@ -53,15 +72,33 @@ app.post('/studentlogin', async (req, res)=>{
         }
     })  
 })
+app.post('/teacherlogin', async (req, res)=>{
+    
+    let {email, password} = req.body
+    let teacher = await teacherModel.findOne({email})
+    if(!teacher) return res.status(500).send("Something went wrong")
+    
+    bcrypt.compare(password, teacher.password, (err, result)=>{
+        if(result){
+            let token = jwt.sign({email: email, teacherid: teacher._id}, "secret")
+            res.cookie("token", token)
+            
+            res.status(200).redirect("/teacherprofile")
+        }
+        else{
+            res.redirect("/teacherlogin")
+        }
+    })  
+})
 
 app.post('/studentregister', async (req,res)=>{
-    let {name, course, intake, email, password} = req.body
-    let user = await studentModel.findOne({email})
-    if(user) res.status(500).send("User Already Created")
+    let {name, email, password} = req.body
+    let student = await studentModel.findOne({email})
+    if(student) res.status(500).send("User Already Created")
     
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(password, salt, async function(err, hash) {
-            let user = await studentModel.create({
+            let student = await studentModel.create({
                 name,
                 email,
                 intake,
@@ -69,6 +106,24 @@ app.post('/studentregister', async (req,res)=>{
                 password: hash
             })
             let token = jwt.sign({email: email, studentid: student._id}, "secret")
+            res.cookie("token", token)
+            res.send("Registered")
+        });
+    });
+})
+app.post('/teacherregister', async (req,res)=>{
+    let {name, email, password} = req.body
+    let teacher = await teacherModel.findOne({email})
+    if(teacher) res.status(500).send("User Already Created")
+    
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, async function(err, hash) {
+            let teacher = await teacherModel.create({
+                name,
+                email,
+                password: hash
+            })
+            let token = jwt.sign({email: email, teacherid: teacher._id}, "secret")
             res.cookie("token", token)
             res.send("Registered")
         });
--- models/teacher.js
+++ models/teacher.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const teacherSchema = mongoose.Schema({
+    name: String,
     email: String,
     password: String
 })
